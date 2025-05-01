47451633c9bf87efa8a16f935322dfc544157b9c
SpooderButt <TheGreedySpooder@Outlook.com>
2025-05-01 01:32:30 -0400

    Fixed issue not playing animation

--- beginning_rpg/player.gd
+++ beginning_rpg/player.gd
@@ -1,16 +1,20 @@
 class_name Player extends CharacterBody2D
 
-var move_speed : float = 100
+var move_speed : float = 1000
 
 func _ready() -> void:
 	pass
 
-func _process(delta: float) -> void:
+func _process(delta):
+	
 	var direction : Vector2 = Vector2.ZERO
 
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 
 	velocity = direction * move_speed
+	
+	pass
 
-	print("hello world")
+func _physics_process( delta ):
+	move_and_slide()
